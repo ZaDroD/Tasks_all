d59b4498a28b8c83f9ce457de5376753be86ff9f
Evgeniy <zadrod93@yandex.ru>
2022-03-07 18:25:19 +0300

    Задача номер 6

---
+++ Task06/Program.cs
@@ -0,0 +1,7 @@
+﻿// 6. Выяснить является ли число чётным
+
+int num = new Random().Next(1,100);
+Console.WriteLine(num);
+if (num % 2 == 0)
+Console.WriteLine("Число " + num + " является чётным");
+else Console.WriteLine("Число " + num + " не является чётным");
