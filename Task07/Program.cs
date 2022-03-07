18b4e76b36eee7467f3765e4210a8b5472ae5cae
Evgeniy <zadrod93@yandex.ru>
2022-03-07 18:47:13 +0300

    Задача номер 7

---
+++ Task07/Program.cs
@@ -0,0 +1,11 @@
+﻿// 7. Показать числа от -N до N
+
+Console.WriteLine("Введите целое число ");
+string number = Console.ReadLine();
+int num = int.Parse(number);
+int numN = num * -1;
+while (numN <= num)
+{
+    Console.WriteLine(numN);
+    numN ++;
+}
