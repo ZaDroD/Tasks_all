d03c2efd4a77dd64f73f6434832d451c066d6376
Evgeniy <zadrod93@yandex.ru>
2022-03-07 20:47:30 +0300

    Задача номер 8

---
+++ Task08/Program.cs
@@ -0,0 +1,12 @@
+﻿// 8. Показать четные числа от 1 до N
+
+Console.WriteLine("Введите целое число N ");
+string number = Console.ReadLine();
+int num = int.Parse(number);
+int count = 0;
+while (count <= num)
+{
+    Console.WriteLine("Числа от 0 до N " + count );
+    count = count + 2;
+
+}
