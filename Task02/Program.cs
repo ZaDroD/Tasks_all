03b440103c3be30795cb4f745b873f804300aa52
Evgeniy <zadrod93@yandex.ru>
2022-03-07 16:51:29 +0300

    Задача номер 2

---
+++ Task02/Program.cs
@@ -0,0 +1,7 @@
+﻿// 2. Даны два числа. Показать большее и меньшее число
+
+int numA = 82;
+int numB = 33;
+
+if (numA > numB) Console.WriteLine(numA + " = большее число " + numB + " = меньшее число");
+else Console.WriteLine(numB + " = большее число " + numA + " = меньшее число");
