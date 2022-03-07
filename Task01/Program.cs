1b91f82e8fc12f22a90e0c98fd8fbc6c9a7137f8
Evgeniy <zadrod93@yandex.ru>
2022-03-07 16:42:33 +0300

    Задача номер 1

---
+++ Task01/Program.cs
@@ -0,0 +1,9 @@
+﻿// 1. По двум заданным числам проверять является ли первое квадратом второго
+
+int numA = 9;
+int numB = 3;
+
+if (numB * numB == numA)
+Console.WriteLine("Число " + numA + " является квадратом числа " + numB );
+else 
+Console.WriteLine("Число " + numA + " не является квадратом числа " + numB );
