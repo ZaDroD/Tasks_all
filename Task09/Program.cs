00829216ccf1c824b6bff94dec59f229e3658965
Evgeniy <zadrod93@yandex.ru>
2022-03-08 15:01:38 +0300

    Задача номер 9

---
+++ Task09/Program.cs
@@ -0,0 +1,6 @@
+﻿// 9. Показать последнюю цифру трёхзначного числа
+
+int num = new Random().Next(100,1000);
+Console.WriteLine("Рандомное трёхзначное число = " + num);
+int lastnum = num % 10;
+Console.WriteLine("Последняя цифра цисла = " + lastnum);
